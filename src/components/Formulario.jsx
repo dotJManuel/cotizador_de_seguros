aafcdfc6c99327ea2c09507f55075e12cb5e6bbe
JoeManuel15 <jsmnlhm@outlook.com>
2023-04-02 19:30:31 -0600

    implementacion de logica para cotizar un seguro

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,11 +1,30 @@
 import { Fragment } from "react"
 import { MARCAS, YEARS, PLANES } from "../constants"
+import useCotizador from "../hooks/useCotizador"
+import Error from "./Error"
 
 export const Formulario = () => {
+
+    const { datos, handleChangeDatos, error, setError, cotizarSeguro } = useCotizador()
+
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        if(Object.values(datos).includes('')){
+            setError('Todos los campos son obligatorios')
+            return
+        }
+
+        setError('')
+        cotizarSeguro()
+    }
+
     return (
       <>
-
-        <form>
+        {error && <Error />}
+        <form
+            onSubmit={handleSubmit}
+        >
             <div className="my-5">
                 <label className="block mb-3 font-bold text-gray-400 uppercase">
                     Marca
@@ -14,6 +33,8 @@ export const Formulario = () => {
                 <select 
                     name="marca"
                     className="w-full bg-white border border-gray-200"
+                    onChange={ e => handleChangeDatos(e) }
+                    value={datos.marca}
                 >
                     <option>-- Selecciona Marca --</option>
 
@@ -34,8 +55,10 @@ export const Formulario = () => {
                 </label>
 
                 <select 
-                    name="marca"
+                    name="year"
                     className="w-full bg-white border border-gray-200"
+                    onChange={ e => handleChangeDatos(e) }
+                    value={datos.year}
                 >
                     <option>-- Selecciona Año --</option>
 
@@ -65,6 +88,7 @@ export const Formulario = () => {
                                 type="radio"
                                 name="plan"
                                 value={plan.id}
+                                onChange={ e => handleChangeDatos(e) }
                             />
                         </Fragment>
                     ))}
--- src/context/CotizadorProvider.jsx
+++ src/context/CotizadorProvider.jsx
@@ -1,13 +1,51 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+import { obtenerDiferenciaYear, calcularMarca, calcularPlan, formatearDinero } from "../helpers";
 
 const CotizadorContext = createContext()
 
 const CotizadorProvider = ({ children }) => {
 
+    const [ datos, setDatos ] = useState({
+        marca: '',
+        year: '',
+        plan: ''
+    })
+
+    const [ error, setError ] = useState('')
+    const [ resultado, setResultado ] = useState(0)
+
+    const handleChangeDatos = e => {
+        setDatos({
+            ...datos,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    const cotizarSeguro = () => {
+
+        let resultado = 2000
+
+        const diferencia = obtenerDiferenciaYear(datos.year)
+
+        resultado -= ((diferencia * 3) * resultado) / 300
+
+        resultado *= calcularMarca(datos.marca)
+
+        resultado *= calcularPlan(datos.plan)
+
+        resultado = formatearDinero(resultado)
+
+        setResultado(resultado)
+    }
+
     return (
         <CotizadorContext.Provider
             value = {{
-                
+                datos,
+                handleChangeDatos,
+                error,
+                setError,
+                cotizarSeguro
             }}
         >
             {children}
